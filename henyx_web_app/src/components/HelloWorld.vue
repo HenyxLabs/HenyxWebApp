776b0c4bc13d58a5ba31dceade0ec72fe1b64fef
David Brun <davidbrunalvarez@gmail.com>
2025-07-09 21:57:02 +0200

    wip

--- henyx_web_app/src/components/HelloWorld.vue
+++ henyx_web_app/src/components/HelloWorld.vue
@@ -1,41 +1,90 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div class="main">
+    <div class="logo">
+      <img src="/src/assets/LogoHenyx.png" alt="Company Logo">
+    </div>
+    <div class="navbar">
+      <nav>
+        <ul>
+          <li><a class="hover-underline" href="#">Blog</a></li>
+          <li><a class="hover-underline" href="#">Projects</a></li>
+          <li><a class="hover-underline" href="#">Services</a></li>
+          <li><a class="hover-underline" href="#">About us</a></li>
+        </ul>
+      </nav>
+    </div>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+.logo {
+  width: 100vw;
+  margin-top: 10vh;
+  margin-bottom: 10vh;
+}
+.logo > img{
+  width: 70vw;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.navbar{
+  width: 100vw;
+  margin-top: 10vh;
+  margin-bottom: 10vh;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  text-align:center;
+}
+
+li {
+  display: inline-block;
+  margin-left: 1vw;
+  margin-right: 1vw;
+}
+
+li > a {
+  text-decoration: none;
+  color: #00ff00;
+  padding: 5px;
+}
+
+.hover-underline {
+  font-size: 1rem;
+  color: #00ff00;
+  position: relative;
+  display: inline-block;
+}
+
+.hover-underline::after,
+.hover-underline::before {
+  content: '';
+  position: absolute;
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(to right, #000000, #00ff00);
+  bottom: -5px;
+  left: 0;
+  transform: scaleX(0);
+  transform-origin: right;
+  transition: transform 0.4s ease-out;
+}
+
+.hover-underline::before {
+  top: -5px;
+  transform-origin: left;
+}
+
+.hover-underline:hover::after,
+.hover-underline:hover::before {
+  transform: scaleX(1);
 }
 </style>
